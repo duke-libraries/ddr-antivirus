fa1aac98e6626ddb4d82fc034c866e3bf0a0f7cc
dchandekstark <dchandekstark@gmail.com>
2014-11-07 17:46:54 -0500

    NullScannerAdapter returns NullScanResult (closes #8) Added test module for NullScannerAdapter. Added YARD doc content to ScannerAdapter.

--- lib/ddr/antivirus/adapters/scanner_adapter.rb
+++ lib/ddr/antivirus/adapters/scanner_adapter.rb
@@ -6,10 +6,15 @@ module Ddr
       # 
       class ScannerAdapter
 
+        # Scan a file path for viruses - subclasses must implement.
+        #
+        # @param [String] file path to scan
+        # @return [Ddr::Antivirus::ScanResult] the result
         def scan(path)
           raise NotImplementedError
         end
 
+        # Return the adapter configuration options
         def config
           Ddr::Antivirus.adapter_config
         end
