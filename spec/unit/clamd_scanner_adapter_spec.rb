6e4744a7208aa2e0f075dd190bc0c542e455703c
David Chandek-Stark <dchandekstark@gmail.com>
2015-11-03 16:32:20 -0500

    Replace usage of Open3 with backticks in clamd adapter.
    Adds integration tests.
    Adds Travis CI config to install and run clamav-daemon.
    Adds EICAR test file.

--- spec/unit/clamd_scanner_adapter_spec.rb
+++ spec/unit/clamd_scanner_adapter_spec.rb
@@ -1,10 +1,9 @@
 require "tempfile"
+require "fileutils"
 require "ddr/antivirus/adapters/clamd_scanner_adapter"
 
 module Ddr::Antivirus
   RSpec.describe ClamdScannerAdapter do
-
-    let(:path) { File.expand_path(File.join("..", "..", "fixtures", "blue-devil.png"), __FILE__) }
 
     before do
       allow(subject).to receive(:version) { "version" }
@@ -15,57 +14,48 @@ module Ddr::Antivirus
         @file = Tempfile.new("test")
         @file.write("Scan me!")
         @file.close
-        allow(subject).to receive(:command).with(@file.path) { ["#{@file.path}: OK", double(exitstatus: 0)] }
+        FileUtils.chmod(0000, @file.path)
       end
       after { @file.unlink }
-      describe "when the file is not world readable" do
-        it "should temporarily change the permissions" do
-          FileUtils.chmod("a-r", @file.path)
-          original_mode = File.stat(@file.path).mode
-          expect(FileUtils).to receive(:chmod).with("a+r", @file.path)
-          subject.scan(@file.path)
-          expect(File.stat(@file.path).mode).to eq(original_mode)
+      describe "when the file is not readable" do
+        it "scans the file" do
+          expect { subject.scan(@file.path) }.not_to raise_error
         end
-      end
-      describe "when the file is world readable" do
-        before { FileUtils.chmod("a+r", @file.path) }
-        it "should not change the permissions" do
-          original_mode = File.stat(@file.path).mode
-          expect(FileUtils).not_to receive(:chmod)
-          subject.scan(@file.path)
-          expect(File.stat(@file.path).mode).to eq(original_mode)
+        it "resets the original permissions" do
+          expect { subject.scan(@file.path) }.not_to change { File.stat(@file.path).mode }
         end
       end
     end
 
     describe "result" do
+      let(:path) { File.expand_path(File.join("..", "..", "fixtures", "blue-devil.png"), __FILE__) }
       before do
-        allow(subject).to receive(:command).with(path) { ["output", status] }
+        allow(subject).to receive(:clamdscan).with(path) { ["output", exitcode] }
       end
       describe "when a virus is found" do
-        let(:status) { double(exitstatus: 1) }
+        let(:exitcode) { 1 }
         it "should raise a VirusFoundError" do
           expect { subject.scan(path) }.to raise_error(VirusFoundError)
         end
       end
       describe "when there is an error" do
-        let(:status) { double(exitstatus: 2) }
-        it "should raise a ScannerError" do
+        let(:exitcode) { 2 }
+        it "raises a ScannerError" do
           expect { subject.scan(path) }.to raise_error(ScannerError)
         end
       end
       describe "success" do
-        let(:status) { double(exitstatus: 0) }
-        it "should have output" do
+        let(:exitcode) { 0 }
+        it "has output" do
           expect(subject.scan(path).output).to eq("output")
         end
-        it "should have a scanned_at time" do
+        it "has a scanned_at time" do
           expect(subject.scan(path).scanned_at).to be_a(Time)
         end
-        it "should have a version" do
+        it "has a version" do
           expect(subject.scan(path).version).to eq("version")
         end
-        it "should have the file_path" do
+        it "has the file_path" do
           expect(subject.scan(path).file_path).to eq(path)
         end
       end
