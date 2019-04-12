df19a8d50d59a6f1bac4d398bd3b0e534b12b01d
David Chandek-Stark <david.chandek.stark@duke.edu>
2019-04-12 14:36:52 -0400

    Increase wait-for-it clamav timeout to 60 sec. Move clamd.conf from entrypoint to Dockerfile.

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Client needs to know server conf :(
-echo "TCPSocket 3310" > /etc/clamav/clamd.conf
-echo "TCPAddr clamav" >> /etc/clamav/clamd.conf
-
-wait-for-it clamav:3310 -t 30 -- "$@"
+wait-for-it clamav:3310 -t 60 -- "$@"
